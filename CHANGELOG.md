fb3df1d1fb9f282984ddb763c0820760940b611e
pengbo43 <pengbo43@jd.com>
2024-01-08 17:28:19 +0800

    release: 1.0.1

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+## 1.0.1 (2024-01-08)
+
+
+### Features
+
+* init ([aa78b4f](https://github.com/PengBoUESTC/vue-version-check/commit/aa78b4f6a597850979191c968fc8a3c7a6539934))
+
+
+
--- dist/index.js
+++ dist/index.js
@@ -1,61 +1,61 @@
-'use strict'
+'use strict';
 
 const defaultBeforeReload = () => {
-  return true
-}
+    return true;
+};
 const formatTest = (test) => {
-  if (!test)
-    return []
-  if (typeof test === 'string')
-    test = [test]
-  if (Array.isArray(test))
-    return test.map(str => new RegExp(`^${str}`))
-  if (test instanceof RegExp)
-    return [test]
-  return []
-}
+    if (!test)
+        return [];
+    if (typeof test === 'string')
+        test = [test];
+    if (Array.isArray(test))
+        return test.map((str) => new RegExp(`^${str}`));
+    if (test instanceof RegExp)
+        return [test];
+    return [];
+};
 const useVersionCheck = (options) => {
-  const { beforeReload = defaultBeforeReload, test } = options
-  const testList = formatTest(test)
-  const run = (state) => {
-    if (state.back) {
-      window.history.back()
-    }
-    else {
-      window.location.reload()
-    }
-  }
-  const listener = () => {
-    window.addEventListener('popstate', (e) => {
-      run(e.state)
-    })
-    run(history.state)
-  }
-  const handleUnhandledrejection = async (e) => {
-    const { message } = e.reason
-    if (/Failed to fetch dynamically imported module|Unable to preload|'text\/html' is not a valid JavaScript MIME type/.test(message) ||
-            testList.find(errorRe => errorRe.test(message))) {
-      e.preventDefault()
-      try {
-        const res = await beforeReload()
-        if (!res)
-          return
-        listener()
-      }
-      catch (e) {
-        console.error('useVersionCheck :: ', e)
-      }
-    }
-  }
-  const addEventListener = () => {
-    window.removeEventListener('unhandledrejection', handleUnhandledrejection)
-    window.addEventListener('unhandledrejection', handleUnhandledrejection)
-  }
-  addEventListener()
-  return {
-    run,
-    listener
-  }
-}
+    const { beforeReload = defaultBeforeReload, test } = options;
+    const testList = formatTest(test);
+    const run = (state) => {
+        if (state.back) {
+            window.history.back();
+        }
+        else {
+            window.location.reload();
+        }
+    };
+    const listener = () => {
+        window.addEventListener('popstate', (e) => {
+            run(e.state);
+        });
+        run(history.state);
+    };
+    const handleUnhandledrejection = async (e) => {
+        const { message } = e.reason;
+        if (/Failed to fetch dynamically imported module|Unable to preload|'text\/html' is not a valid JavaScript MIME type/.test(message) ||
+            testList.find((errorRe) => errorRe.test(message))) {
+            e.preventDefault();
+            try {
+                const res = await beforeReload();
+                if (!res)
+                    return;
+                listener();
+            }
+            catch (e) {
+                console.error('useVersionCheck :: ', e);
+            }
+        }
+    };
+    const addEventListener = () => {
+        window.removeEventListener('unhandledrejection', handleUnhandledrejection);
+        window.addEventListener('unhandledrejection', handleUnhandledrejection);
+    };
+    addEventListener();
+    return {
+        run,
+        listener,
+    };
+};
 
-exports.useVersionCheck = useVersionCheck
+exports.useVersionCheck = useVersionCheck;
